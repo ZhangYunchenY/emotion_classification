5136fb070f65b4b6d1507d0277b95d5a55bef2d4
ZhangYunchenY <yunchenx@outlook.com>
2021-03-28 11:21:15 +0800

    update

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
+import os
 import sys
-
 sys.path.append('..')
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import numpy as np
 import torch.nn as nn
 from sklearn import metrics
@@ -10,16 +11,6 @@ from torch.utils.tensorboard import SummaryWriter
 from transformers import BertConfig, AdamW, get_linear_schedule_with_warmup
 
 
-EPOCH = 2
-BATCH_SIZE = 16
-TARGET = '_qa_w'
-LOG_PATH = './log'
-MODEL_NAME = 'hfl/chinese-roberta-wwm-ext-large'
-TRAIN_PATH = './data/train_features' + TARGET + '.pkl'
-DEV_PATH = './data/dev_features' + TARGET + '.pkl'
-MODEL_SAVE_PATH = './roberta_model_save_file/motion' + TARGET + '.pt'
-
-
 def train(train_dataloader, dev_dataloader):
     print('===== Loading model... =====')
     config = BertConfig.from_pretrained(MODEL_NAME)
@@ -27,7 +18,7 @@ def train(train_dataloader, dev_dataloader):
     model.cuda()
     # optimizer and scheduler
     total_step = EPOCH * len(train_dataloader)
-    optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01)
+    optimizer = AdamW(model.parameters(), lr=2e-6, weight_decay=0.01)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=10, num_training_steps=total_step)
     # tensor board
     tensorboard_writer = SummaryWriter(LOG_PATH)
@@ -47,7 +38,7 @@ def train(train_dataloader, dev_dataloader):
                            attention_mask=b_attention_masks)
             loss = output
             epoch_loss += loss.item()
-            tensorboard_writer.add_scalar('train_loss', epoch_loss / (step + 1),
+            tensorboard_writer.add_scalar(TARGET + 'train_loss', epoch_loss / (step + 1),
                                           step + i * len(train_dataloader))
             tensorboard_writer.flush()
             # loss = loss ** 2
@@ -113,25 +104,41 @@ def train(train_dataloader, dev_dataloader):
         precision = metrics.precision_score(dev_features.ce_labels, predictions, average='binary')
         recall = metrics.recall_score(dev_features.ce_labels, predictions, average='binary')
         f1 = metrics.f1_score(dev_features.ce_labels, predictions, average='binary')
-        tensorboard_writer.add_scalar('dev_epoch_loss', dev_epoch_loss, dev_loss_epoch_count)
-        tensorboard_writer.add_scalar('dev_precision', precision, dev_loss_epoch_count)
-        tensorboard_writer.add_scalar('dev_recall', recall, dev_loss_epoch_count)
-        tensorboard_writer.add_scalar('dev_f1', f1, dev_loss_epoch_count)
+        accuracy = metrics.accuracy_score(dev_features.ce_labels, predictions)
+        tensorboard_writer.add_scalar(TARGET + 'dev_epoch_loss', dev_epoch_loss, dev_loss_epoch_count)
+        tensorboard_writer.add_scalar(TARGET + 'dev_acc', accuracy, dev_loss_epoch_count)
+        tensorboard_writer.add_scalar(TARGET + 'dev_precision', precision, dev_loss_epoch_count)
+        tensorboard_writer.add_scalar(TARGET + 'dev_recall', recall, dev_loss_epoch_count)
+        tensorboard_writer.add_scalar(TARGET + 'dev_f1', f1, dev_loss_epoch_count)
         tensorboard_writer.flush()
         dev_loss_epoch_count += 1
     # complete
     torch.save(model.state_dict(), MODEL_SAVE_PATH)
 
 
+TARGET_LIST = ['_lack_of_anger', '_lack_of_disgust', '_lack_of_fear', '_lack_of_happiness', '_lack_of_like',
+               '_lack_of_sadness', '_lack_of_surprise']
+
+
 if __name__ == '__main__':
-    train_features = feature_reader(TRAIN_PATH)
-    dev_features = feature_reader(DEV_PATH)
-    train_dataloader = creat_dataloader(BATCH_SIZE, train_features.input_ids,
-                                        train_features.attention_masks,
-                                        train_features.token_type_ids,
-                                        train_features.bce_labels)
-    dev_dataloader = creat_dataloader(BATCH_SIZE, dev_features.input_ids,
-                                      dev_features.attention_masks,
-                                      dev_features.token_type_ids,
-                                      dev_features.bce_labels)
-    train(train_dataloader, dev_dataloader)
+    for TARGET in TARGET_LIST:
+        EPOCH = 2
+        BATCH_SIZE = 17
+        TARGET = TARGET
+        LOG_PATH = './log'
+        MODEL_NAME = 'hfl/chinese-roberta-wwm-ext-large'
+        TRAIN_PATH = './data/train_features' + TARGET + '.pkl'
+        DEV_PATH = './data/dev_features' + TARGET + '.pkl'
+        MODEL_SAVE_PATH = './roberta_model_save_file/motion' + TARGET + '.pt'
+        train_features = feature_reader(TRAIN_PATH)
+        dev_features = feature_reader(DEV_PATH)
+        train_dataloader = creat_dataloader(BATCH_SIZE, train_features.input_ids,
+                                            train_features.attention_masks,
+                                            train_features.token_type_ids,
+                                            train_features.bce_labels)
+        dev_dataloader = creat_dataloader(BATCH_SIZE, dev_features.input_ids,
+                                          dev_features.attention_masks,
+                                          dev_features.token_type_ids,
+                                          dev_features.bce_labels)
+
+        train(train_dataloader, dev_dataloader)
